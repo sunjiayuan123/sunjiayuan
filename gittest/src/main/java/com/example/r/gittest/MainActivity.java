8d2f6357baee4fe605465580edae161de86ee16d
sunjiayuan123 <2772391183@qq.com>
2019-01-18 16:14:56 +0800

    gittest文件上传

--- gittest/src/main/java/com/example/r/gittest/MainActivity.java
+++ gittest/src/main/java/com/example/r/gittest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.r.gitwenjia;
+package com.example.r.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
